e97f6d227c5a714ea8edd8a18f3893ce4b5156cb
JahmiPoulain <j.poulain@rubika-edu.re>
2025-10-03 20:17:52 +0400

    universal projectile v1

---
+++ Assets/UniversalProjectileScript.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class UniversalProjectileScript : MonoBehaviour
+{
+    [Header("degats")]
+    public float baseDamage;
+    public float damageToDeal;
+    [Header("mouvement")]
+    public float velocity;
+    [Header("collision")]
+    public SphereCollider sphereCollider;
+    public int hitsBeforeDestroy;
+    public int currentHitsAmount;
+    [Header("tete chercheuse")]
+    public GameObject target;
+    [Header("explosion")]
+    public float explosionRadius;
+    [Header("taille")]
+    public float size;
+    //[Header("destruction")]
+
+    void Start()
+    {
+        sphereCollider = GetComponent<SphereCollider>();
+        damageToDeal = baseDamage;
+        gameObject.transform.localScale = new Vector3(size, size, size); // modifier la taille de l'objet
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * velocity * Time.deltaTime; // avancer devant
+        if (target != null && currentHitsAmount == 0) // si il y a une cible et qu'il n'a rien touche
+        {
+            SeekTarget();
+        }
+    }
+
+    void OnTriggerEnter(Collider collision) // le collider est un trigger pour passer a travers les ennemis
+    {
+        sphereCollider.radius = size * explosionRadius; // l'explosion c'est juste faire grossir la hitbox de base
+        currentHitsAmount++; // compter le nombre de collision
+        if (currentHitsAmount >= hitsBeforeDestroy) // detruire l'objet si il est a court de collision
+        {
+            InitiateDestruction();
+        }
+        if (collision.gameObject.GetComponent<UniversalEnemyScript>()) // verifier l'identite de l'objet touche
+        {
+            collision.gameObject.GetComponent<UniversalEnemyScript>().currentHealthPoint -= damageToDeal; // infliger des degats a l'objet touche
+        }
+    }
+
+    void SeekTarget()
+    {
+        transform.LookAt(target.transform.position); // s'oriente vers la position de la cible
+    }
+
+    void InitiateDestruction() // detruire
+    {
+        Destroy(gameObject);
+    }
+}
